967d7ad8cd980d38c7e550f40a0a8efd5ba0f9eb
Stefan Ogonek <stefan.ogonek@gmail.com>
2018-11-10 20:46:49 +0100

    Add routes for needed api methods.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :developers, only: %i[index show create]
+
+      resources :projects, only: %i[index show create] do
+        patch 'assign_developer', on: :member
+        patch 'unassign_developer', on: :member
+      end
+    end
+  end
 end
